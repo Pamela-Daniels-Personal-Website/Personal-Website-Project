dd0e9558d16898d2cdf4e9cdd5a0ecf40b9f5a1c
Pamela <pdaniels@Desktop-HomeOffice.localdomain>
2023-09-16 15:45:42 -0400

    added javascript to display a message

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// javascript.js
+
+// Function to display a message
+function showMessage() {
+    alert("Click the image to view the Project!");
+}
+
+// Listener triggers the function on page load
+document.addEventListener("DOMContentLoaded", showMessage);
